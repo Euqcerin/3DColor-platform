4afd0074ef97aeba85d5c9a21dbec9c73f172e77
Fredrik <f.wigarde@hotmail.com>
2015-05-04 19:34:46 +0200

    Base libraries

---
+++ ContentLibrary/FontLibrary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace ContentLibrary
+{
+    class FontLibrary
+    {
+
+        public static ContentManager Content;
+
+        /// <summary>
+        /// Gets a font
+        /// </summary>
+        /// <param name="sName">Name of the font</param>
+        /// <returns>Returns font</returns>
+        public static SpriteFont GetFont(string sName)
+        {
+            SpriteFont font = null;
+            font = Content.Load<SpriteFont>(sName);
+            return font;
+        }
+    }
+}
---
+++ ContentLibrary/TextureLibrary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ContentLibrary
+{
+    public class TextureLibrary
+    {
+        public static ContentManager Content;
+
+        /// <summary>
+        /// Gets a texture
+        /// </summary>
+        /// <param name="sName">Name of the texture</param>
+        /// <returns>Returns texture</returns>
+        public static Texture2D GetTexture(string sName)
+        {
+            Texture2D texture = null;
+            texture = Content.Load<Texture2D>(sName);
+            return texture;
+        }
+    }
+}
