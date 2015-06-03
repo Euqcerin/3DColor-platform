dde66e0e5bf78bac1eb3a3b04576fa69c81d9ce2
Fredrik <f.wigarde@hotmail.com>
2015-06-03 22:49:53 +0200

    LevelSelect

---
+++ 3DColor/3DColor/State/LevelSelect.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3DColor.State
+{
+    class LevelSelect : BaseState
+    {
+
+        public LevelSelect() : base() { 
+        
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gt)
+        {
+            base.Update(gt);
+        }
+
+        public override void Draw(SpriteBatch sb)
+        {
+            base.Draw(sb);
+        }
+    }
+}
