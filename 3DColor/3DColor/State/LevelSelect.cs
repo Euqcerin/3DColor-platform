3c66f3726a57fac020ef3c403a3e78845751bd2d
Fredrik <f.wigarde@hotmail.com>
2015-06-04 22:35:32 +0200

    LevelSelect cont.

--- 3DColor/3DColor/State/LevelSelect.cs
+++ 3DColor/3DColor/State/LevelSelect.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using ContentLibrary;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,42 @@ namespace _3DColor.State
     class LevelSelect : BaseState
     {
 
-        public LevelSelect() : base() { 
-        
+        struct LevelRect {
+
+            public Texture2D t_texture {
+                get { return t_texture; }
+                private set { t_texture = value; }
+            }
+
+            public Rectangle t_rectangle {
+                get { return t_rectangle; }
+                private set { t_rectangle = value; }
+            }
+
+            public LevelRect(int pos, Texture2D texture) {
+                t_rectangle = new Rectangle(0, pos * (Values.SCREEN_HEIGHT / 15) + pos * (Values.SCREEN_HEIGHT / 30), Values.SCREEN_WIDTH / 3, Values.SCREEN_HEIGHT / 15);
+                t_texture = texture;
+            }
+
+        }
+
+        private const string SELECT_1 = "LevelSelect/SelectBackground1";
+        private const string SELECT_2 = "LevelSelect/SelectBackground2";
+
+        private const int t_number_of_levels = 7;
+        private List<LevelRect> Levels = new List<LevelRect>();
+        public Texture2D text_background_select_1;
+        public Texture2D text_background_select_2;
+
+        public LevelSelect() : base() {
+            LoadContent();
+            InitLevels();
         }
 
         public override void LoadContent()
         {
+            text_background_select_1 = TextureLibrary.GetTexture(SELECT_1);
+            text_background_select_2 = TextureLibrary.GetTexture(SELECT_2);
             base.LoadContent();
         }
 
@@ -26,7 +57,20 @@ namespace _3DColor.State
 
         public override void Draw(SpriteBatch sb)
         {
+            for (int i = 0; i < t_number_of_levels; i++)
+                sb.Draw(Levels[i].t_texture, Levels[i].t_rectangle, Color.White);
             base.Draw(sb);
+        }
+
+        private void InitLevels()
+        {
+            for (int i = 0; i < t_number_of_levels; i++)
+            { 
+                if(i%2 == 0)
+                    Levels.Add(new LevelRect(i, text_background_select_1));
+                else
+                    Levels.Add(new LevelRect(i, text_background_select_2));
+            }
         }
     }
 }
